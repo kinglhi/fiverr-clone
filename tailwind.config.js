f5fa95fbf8d173826f09e1bb81aea65f5cf02313
Ner Karso <nerpkarso@gmail.com>
2021-05-26 13:52:28 +0530

    Migrate to Parcel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,16 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: {
-    enabled: true,
-    content:  ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
-  },
+  purge: ['./src/**/*.{js,jsx}', './public/index.html'],
   darkMode: 'class',
   theme: {
     extend: {
       colors: {
         base: colors.coolGray,
         primary: colors.green,
-      }
+      },
     },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+  plugins: [],
 };
